75b92948d6c5b4cc729c8df78062492f4f139b65
John Vundi <johnsonvundy2@gmail.com>
2023-06-23 00:13:49 +0300

    configured usersSlice

--- src/store/slices/UsersSlice.js
+++ src/store/slices/UsersSlice.js
@@ -8,9 +8,17 @@ const usersSlice = createSlice({
         error: null,
     },
     extraReducers(builder){
-        builder.addCase(fetchUsers.pending);
-        builder.addCase(fetchUsers.fulfilled);
-        builder.addCase(fetchUsers.rejected);
+        builder.addCase(fetchUsers.pending, (state, action) => {
+            state.isLoading = true;
+        });
+        builder.addCase(fetchUsers.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.data = action.payload;
+        });
+        builder.addCase(fetchUsers.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action.error
+        });
     }
 });
 
